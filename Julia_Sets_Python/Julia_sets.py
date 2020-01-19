9933576ad900e9725f04af4e1a8ada2ffd797497
Errol R. Bucy <erb@Errols-MacBook-Pro.local>
2020-01-18 20:22:35 -0800

    New Code in Notebook. Not Dumb.

--- Julia_Sets_Python/Julia_sets.py
+++ Julia_Sets_Python/Julia_sets.py
@@ -1,49 +1,78 @@
-import numpy as np
+"""
+Solution from:
+https://codereview.stackexchange.com/questions/210271/generating-julia-set
+"""
+from functools import partial
+from numbers import Complex
+from typing import Callable
+ 
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-
-class Julia: 
-
-    def __init__(self, im_width = 500, im_height = 500, zabs_max = 10, nit_max =1000, xmin = -1.5 , xmax = 1.5, ymin = -1.5, ymax = 1.5, c = complex(0.0, 0.0)):
-        self.im_width, self.im_height = im_width, im_height
-        self.c = c
-        self.zabs_max = zabs_max
-        self.nit_max = nit_max
-        self.xmin, self.xmax = xmin, xmax
-        self.xwidth = self.xmin - self.xmax
-        self.ymin, self.ymax = ymin, ymax
-        self.yheight = self.ymin - self.ymax
-        self.julia = np.zeros((self.im_width, self.im_height))
-        for ix in range(self.im_width):
-            for iy in range(self.im_height):
-                nit = 0
-                # Map pixel position to a point in the complex plane
-                self.z = complex(ix / self.im_width * self.xwidth + self.xmin,
-                                iy / self.im_height * self.yheight + self.ymin)
-                # Do the iterations
-                while abs(self.z) <= self.zabs_max and nit < self.nit_max:
-                    self.z = self.z**2 + self.c
-                    nit += 1
-                shade = 1-np.sqrt(nit / self.nit_max)
-                ratio = nit / self.nit_max
-                self.julia[ix, iy] = ratio
-    
-    def plot(self): 
-        fig, ax = plt.subplots()
-        ax.imshow(self.julia, interpolation='nearest', cmap=cm.hot)
-        # Set the tick labels to the coordinates of z0 in the complex plane
-        xtick_labels = np.linspace(self.xmin, self.xmax, self.xwidth / 0.5)
-        ax.set_xticks([(x-self.xmin) / self.xwidth * self.im_width for x in xtick_labels])
-        ax.set_xticklabels(['{:.1f}'.format(xtick) for xtick in xtick_labels])
-        ytick_labels = np.linspace(self.ymin, self.ymax, self.yheight / 0.5)
-        ax.set_yticks([(y-self.ymin) / self.yheight * self.im_height for y in ytick_labels])
-        ax.set_yticklabels(['{:.1f}'.format(ytick) for ytick in ytick_labels])
-        plt.show()
-
-Terrain = Julia(c = complex(0.65, 1.3))
-Terrain.plot()
-
-
+import numpy as np
+ 
+ 
+def douady_hubbard_polynomial(z: Complex,
+                              c: Complex) -> Complex:
+    """
+    Monic and centered quadratic complex polynomial
+    https://en.wikipedia.org/wiki/Complex_quadratic_polynomial#Map
+    """
+    return z ** 2 + c
+ 
+ 
+def julia_set(mapping: Callable[[Complex], Complex],
+              *,
+              min_coordinate: Complex,
+              max_coordinate: Complex,
+              width: int,
+              height: int,
+              iterations_count: int = 256,
+              threshold: float = 2.) -> np.ndarray:
+    """
+    As described in https://en.wikipedia.org/wiki/Julia_set
+    :param mapping: function defining Julia set
+    :param min_coordinate: bottom-left complex plane coordinate
+    :param max_coordinate: upper-right complex plane coordinate
+    :param height: pixels in vertical axis
+    :param width: pixels in horizontal axis
+    :param iterations_count: number of iterations
+    :param threshold: if the magnitude of z becomes greater
+    than the threshold we assume that it will diverge to infinity
+    :return: 2D pixels array of intensities
+    """
+    im, re = np.ogrid[min_coordinate.imag: max_coordinate.imag: height * 1j,
+                      min_coordinate.real: max_coordinate.real: width * 1j]
+    z = (re + 1j * im).flatten()
+ 
+    live, = np.indices(z.shape)  # indexes of pixels that have not escaped
+    iterations = np.empty_like(z, dtype=int)
+ 
+    for i in range(iterations_count):
+        z_live = z[live] = mapping(z[live])
+        escaped = abs(z_live) > threshold
+        iterations[live[escaped]] = i
+        live = live[~escaped]
+        if live.size == 0:
+            break
+    else:
+        iterations[live] = iterations_count
+ 
+    return iterations.reshape((height, width))
+ 
+ 
+if __name__ == '__main__':
+    mapping = partial(douady_hubbard_polynomial,
+                      c=-0.7 + 0.27015j)  # type: Callable[[Complex], Complex]
+ 
+    image = julia_set(mapping,
+                      min_coordinate=-1.5 - 1j,
+                      max_coordinate=1.5 + 1j,
+                      width=800,
+                      height=600)
+    plt.axis('off')
+    plt.imshow(image,
+               cmap='nipy_spectral_r',
+               origin='lower')
+    plt.show()
 
 
 
